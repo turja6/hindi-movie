43a7f10bb2122453b1d20bb765e2f190508d7d73
turja6 <kalidash1256@gmail.com>
2025-02-07 17:30:47 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-
+// Example: Add interactivity such as hover effects
+document.querySelectorAll('.movie-item').forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.style.transform = 'scale(1.05)';
+  });
+  
+  item.addEventListener('mouseout', () => {
+    item.style.transform = 'scale(1)';
+  });
+});
